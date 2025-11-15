c90729d5eaf22592281fb8dcd36042fa82d459e6
BaharDori <bahar2bahar@gmail.com>
2025-11-14 22:32:36 -0800

    Capstone

---
+++ Finalsv2/story_content/triggers.js
@@ -0,0 +1,60 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6glESmCrnb4":
+        Script1();
+        break;
+      case "6EplaXLk2q0":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('62hgHtlDil9');
+const duration = 750;
+const easing = 'ease-out';
+const id = '62KoaJ3YR1H';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('5YemqHchGBA');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6GfmBEudfNg';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
