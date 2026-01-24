7156dad7d3e0fffca37727e72cda29b0518ab94f
BaharDori <bahar2bahar@gmail.com>
2026-01-23 18:41:14 -0800

    cap

---
+++ ArabicrootsFinal/story_content/triggers.js
@@ -0,0 +1,46 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5ftBsggl7Kt":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  const target = object('6SVVId1U5mQ');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6B8rHbnfdHD';
+player.addForTriggers(
+id,
+target.animate(
+[ {opacity: 1 }, 
+{opacity: 0 }, 
+{opacity: 1 }, 
+{opacity: 0 }, 
+{opacity: 1 } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
